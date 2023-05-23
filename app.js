75a2fa1f773b285e8ae80087562e84bf7acc85b1
andres p <andresfpaternina@gmail.com>
2023-05-23 10:58:27 -0500

    commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 App({
   onLaunch(options) {
-    // 第一次打开
-    // options.query == {number:1}
-    console.info('App onLaunch');
+    console.info("App onLaunch");
   },
-  onShow(options) {
-    // 从后台被 scheme 重新打开
-    // options.query == {number:1}
-  },
+  onShow(options) {},
+    globalData: {
+    DocumentType:1,//1 es cc ,si es 2 es nit 
+    tipoCanalID:"10",
+    email:"evoluciondecanales@gmail.com",
+    urlActivate:'https://portalvoz.claro.com.co/',
+    nombre:"Sistema Calle telmex Col",
+    apellido:"",
+    UserProfileID:"evoluciondecanales@gmail.com",
+    DocumentNumber: "56220783",
+    NumberAccount: "56220783",
+    lineNumber: "3058863237",
+    sessionId:
+      "U2FsdGVkX1/M2RqDOPvv79ABiYicxpWEV2ZHcpMyIgu8VqqxGENzU66PQb2vbE+YiGJe446unXTqdvfe45IE3w56zH84KcUlranzt6HctUjI2g6YMrIuv2jmPn9VhiEo7ZlbI/ZHekMtXO4OWdYsRvtfuOBY1x0PFqzYbyjm0D3I7csjXWIRxpoehfi8/A5CXjn4fBj/dZ4is8dQtt92hJo+CHOWlc+Ou0193ZeQ882ExRUksytOIxuSPlP93aiOmyYD0xCbD5O+Jfiyi4UDT6EBHSQhBqV3cwVcbBavoQ2xVpL/HB0UuRigwfTpc+H7CMZEaT6U4JrSlkdHhI0hdo7r1vVtTdVCAWlweCWUMwR/SrW26kKP15OtuOLZk8VPkA833AwU3Vo+77LkkNXrOLzBBdgskjY1WqJsDwgKo4ZS3z9MlqbA7v9e2X1uxyE+DD66Vee3vHSrhM8vc/DIms0gLKqDzNaUnBK04/EkGmu7C5yZkKiAiz03xkHRGWUGUnMgQmZ8hz5lFlj9NfpUxyomoiDpqZ05ItoGDEM9T2uNhUQY4G8Y/4HBMH9aB9h8Fugm+hRbx5OxnyuZNDVg3vGA5QhmEIQPB3NQTM4h+bHQfHCfq4F1yCH1liG2kgvaVP6w0zWEsANKuc3ia/qY0xqeKJ/2w3i4KwR30ytZNIGmHEOkJc/gVAt+1tv+ObzxirFJofVTSaZREq8iMbWzlDgiwBIpdBe7c4nwjtigYHrEh+wXTYHDU6Qr4t1BrouoYkJbn/QT2+h7f5/6NXUHBkPlEga4ytRKtoZOGKkWEGhj5COuhFbgEKdFXeUG/bhBNyUZ4eREsp+phhmB8d56wnvJQ+uhC8VVfrqthV7lAzc6BR398jprO+KEKq7+LQuEAYkQHRRoONJQcfp1Hq5KMNCGd2YcBDSCKkrIVooTNus=",
+  }
 });
